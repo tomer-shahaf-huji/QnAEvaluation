f87a254cc15ddcdd2f6fbb38fb04ee1c870282e9
tomer_shahaf <tomer.shahaf@mail.huji.ac.il>
2023-08-08 12:03:36 +0300

    openaiembeddings works

--- src/main.py
+++ src/main.py
@@ -2,6 +2,6 @@ from src.llms_api.openai_client import OpenAIClient
 
 if __name__ == '__main__':
     client = OpenAIClient()
-    prompt = "What is the capital of Spain?"
+    prompt = "What is the capital of Italy?"
     response = client.generate_response(prompt)
     print(response)
