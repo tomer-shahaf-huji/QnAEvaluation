6a3814abb300849eeea4255a148b6c1c412d98f1
elad619 <elad619@gmail.com>
2023-08-17 16:00:35 +0300

    added splitters

--- src/utils/file_extension_utils.py
+++ src/utils/file_extension_utils.py
@@ -4,9 +4,5 @@ FILE_NAME_INDEX = 0
 FILE_EXTENSION_INDEX = 1
 
 
-def remove_file_extension(file_path: str) -> str:
-    return os.path.splitext(file_path)[FILE_NAME_INDEX]
-
-
 def extract_file_extension(file_path: str) -> str:
     return os.path.splitext(file_path)[FILE_EXTENSION_INDEX]
